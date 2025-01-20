ca2a1c8af9a497594c180cf19b3a5fad60549043
Farzy820 <f.faruqi820@gmail.com>
2025-01-20 12:52:25 -0700

    Update keymap.c

--- keymap.c
+++ keymap.c
@@ -1,7 +1,10 @@
+// Each layer has its own layout and can be toggled, allowing a button to be 
+// a different input depending on the active layer. This enables you to switch 
+// between different keymaps easily.
+
 #include QMK_KEYBOARD_H
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [0] = LAYOUT(
+    [0] = LAYOUT( //Layer 1
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
@@ -9,7 +12,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,    KC_NO,    KC_NO,    KC_NO
     ),
 
-    [1] = LAYOUT(
+    [1] = LAYOUT( //Layer 2
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
@@ -17,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,    KC_NO,    KC_NO,    KC_NO
     ),
 
-    [2] = LAYOUT(
+    [2] = LAYOUT( //Layer 3
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
@@ -25,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_NO,    KC_NO,    KC_NO,    KC_NO
     ),
 
-    [3] = LAYOUT(
+    [3] = LAYOUT( //Layer 4
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
         KC_NO,    KC_NO,    KC_NO,    KC_NO,
